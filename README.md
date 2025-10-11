f7324804c8fa980b9a3f924e5cb826b12cc9f858
Yuesubi <yuesubi.hen@gmail.com>
2025-10-11 21:40:09 +0200

    Add nix flake

---
+++ README.md
@@ -0,0 +1,20 @@
+# PiiXel Engine
+
+## Building
+
+### Linux
+Dependencies:
+```txt
+cmake raylib libxcursor libxrandr xorg-libxi xorg-libxinerama
+```
+(the exact names of these packages will vary across different distos)
+
+Building:
+```sh
+mkdir build
+cd build
+cmake ..
+cmake --build .
+cd Debug
+./<name>
+```
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Flake for the PiiXel Engine";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        # packages = {};
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cmake
+            # glfw
+            raylib
+
+            libxcursor
+            libxrandr
+            xorg.libXi
+            xorg.libXinerama
+          ];
+        };
+      }
+    );
+
+}
